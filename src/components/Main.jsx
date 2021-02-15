222d97bebf36668403c057fe8421cddefeaea782
Dilmurod3 <rahimoff.dilmurod@gmail.com>
2021-02-15 18:42:36 +0400

    test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import "./Main";
-import { Route, Switch } from "react-router-dom";
-import About from "../mainpages/About";
-// import Projects from "../mainpages/Projects";
-// import Contact from "../mainpages/Contact";
+// import React from "react";
+// import "./Main";
+// import { Route, Switch } from "react-router-dom";
+// import About from "../mainpages/About";
+// // import Projects from "../mainpages/Projects";
+// // import Contact from "../mainpages/Contact";
 
-function Main() {
-  return (
-    <Switch location={window.location} key={window.location.pathname}>
-      <Route exact path="/" component={About} />
-      <Route path="/projects" component={() => <div>Project</div>} />
-      <Route path="/contact" component={() => <div>contact</div>} />
-    </Switch>
-  );
-}
+// function Main() {
+//   return (
+//     <Switch location={window.location} key={window.location.pathname}>
+//       <Route exact path="/" component={About} />
+//       <Route path="/projects" component={() => <div>Project</div>} />
+//       <Route path="/contact" component={() => <div>contact</div>} />
+//     </Switch>
+//   );
+// }
 
-export default Main;
+// export default Main;
